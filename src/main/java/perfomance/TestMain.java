8318c5b62c551eecb694aa7acfde949ac0f0eca0
wangkuixing <wangkx0902@163.com>
2020-01-08 17:28:43 +0800

    remove

--- src/main/java/perfomance/TestMain.java
+++ src/main/java/perfomance/TestMain.java
@@ -10,17 +10,5 @@ public class TestMain {
     public static void main(String[] args){
         Commons config = Commons.load("/globalConfig.yaml");
         System.out.println(config.mconfig.phoneid);
-
-        //获取当前时间，并转换为java.sql.Time格式
-        java.util.Date udate=new java.util.Date();
-        SimpleDateFormat format=new SimpleDateFormat("HH:mm:ss");
-        String sdate=format.format(udate);
-        Time time = null;
-        try {
-            time = new Time(format.parse(sdate).getTime());
-            System.out.println(time);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
     }
 }
